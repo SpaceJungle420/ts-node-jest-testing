c06b85e04f3948da27ff750ffdd65cb41b07ed3e
SpaceJungle <46966551+SpaceJungle420@users.noreply.github.com>
2025-06-28 16:00:37 -0300

    Jest assertions and matchers

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,15 +1,42 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("utils test suite", () => {
   it("should return uppercase of a valid string", () => {
-    // arrange:
     const sut = toUpperCase;
     const expected = "ABC";
 
-    // act:
     const actual = sut("abc");
 
-    // assert:
     expect(actual).toBe(expected);
+  });
+
+  it("should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+    expect(actual.characters).toContain<string>("M");
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["M", "y", "-", "S", "t", "r", "i", "n", "g"])
+    );
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined;
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy;
   });
 });
